b31431b3449060830f3fb62c56aa937f5c0b724c
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2023-04-20 00:42:51 -0300

    correção erro

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,16 +3,11 @@ import Login from "../screens/login";
 import Cadastro from "../screens/cadastro";
 import Home from "../screens/home";
 import PaginaNaoEncontrada from "../screens/pagina-nao-encontrada";
-import { App2 } from "../components/home/teste";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Login />,
-    },
-    {
-        path: "teste",
-        element: <App2 />,
     },
     {
         path: "cadastro",
