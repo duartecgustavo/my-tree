248decf1db7f701500549b64fea6131706774184
Gustavo <duartecgustavo@outlook.com>
2023-11-11 23:09:16 -0300

    tela de progresso

--- src/components/home/home-navbar.js
+++ src/components/home/home-navbar.js
@@ -1,86 +1,87 @@
-import styled from 'styled-components';
-import { Colors } from '../../styles/design.tokens';
-import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+import { Colors } from "../../styles/design.tokens";
+import { NavLink } from "react-router-dom";
+import profile from "../../assets/profile.svg";
 
 const NavbarStyle = styled.div`
-    height: 100px;
-    width: 100%;
-    
+  height: 100px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  background-color: #00afef44;
+  position: absolute;
+  top: 0;
+
+  p {
+    color: ${Colors.WHITE};
+    font-weight: lighter;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  a {
     display: flex;
     align-items: center;
-    background-color: #00afef44;
-    position: absolute;
-    top: 0;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 2.5rem;
+    color: ${Colors.WHITE};
 
-    p {
-        color: ${Colors.WHITE};
-        font-weight: lighter;
+    &:hover {
+      color: ${Colors.WHITE_GRAY};
     }
+  }
 
-    div {
-        display: flex;
-        justify-content: space-between 
-    }
+  .h1 {
+    color: ${Colors.BLACK_GRAY};
+  }
 
+  .h1:hover {
+    color: ${Colors.WHITE};
+    cursor: cell;
+    font-weight: bold;
+  }
+
+  .links {
+    gap: 30px;
+  }
+
+  .links > a {
+    font-size: 28px;
+  }
+
+  @media (max-width: 768px) {
     a {
-        display: flex;
-        align-items: center;
-        text-decoration: none;
-        font-weight: bold;
-        font-size:  2.5rem;
-        color: ${Colors.WHITE};
-        
-        &:hover {
-            color: ${Colors.WHITE_GRAY};
-        }
+      font-size: 1.5rem;
     }
 
     .h1 {
-        color: ${Colors.BLACK_GRAY};
+      font-size: 1.5rem;
     }
-
-    .h1:hover {
-        color: ${Colors.WHITE};
-        cursor: cell;
-        font-weight: bold;
-    }
-
-    .links{
-        gap: 30px;
-    }
-
-    .links > a{
-        font-size: 28px;
-    }
-
-    @media (max-width: 768px) {
-
-        a {
-            font-size: 1.5rem;
-        }
-
-        .h1 {
-            font-size: 1.5rem;
-        }
-    }
+  }
 `;
 
 export default function Navbar() {
-    return (
-        <NavbarStyle>
-            <div className="container">
-                <NavLink className="h1" to="/home">
-                    myTree
-                </NavLink>
-                <div className="links">
-                    <NavLink to="/home/informacoes-de-cuidado">
-                        Cultivar
-                    </NavLink>
-                    <NavLink to="/">
-                        Login/Cadastre-se
-                    </NavLink>
-                </div>
+  return (
+    <NavbarStyle>
+      <div className="container">
+        <NavLink className="h1" to="/home">
+          myTree
+        </NavLink>
+        <div className="links">
+          <NavLink to="/home/informacoes-de-cuidado">Cultivar</NavLink>
+          <NavLink to="/">Login/Cadastre-se</NavLink>
+          <NavLink to="/home/profile">
+            <div style={{ display: "flex", gap: "6px" }}>
+              Profile <img style={{ width: 36 }} src={profile} />
             </div>
-        </NavbarStyle>
-    )
+          </NavLink>
+        </div>
+      </div>
+    </NavbarStyle>
+  );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,29 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../screens/login";
-import Cadastro from "../screens/cadastro";
 import Home from "../screens/home";
-import PaginaNaoEncontrada from "../screens/pagina-nao-encontrada";
 import InformacoesDeCuidado from "../screens/informacoes-de-cuidado";
+import Login from "../screens/login";
+import PaginaNaoEncontrada from "../screens/pagina-nao-encontrada";
+import Profile from "../screens/profile";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login />,
-    },
-    {
-        path: "cadastro",
-        element: <Cadastro />,
-    },
-    {
-        path: "home",
-        element: <Home />,
-    },
-    {
-        path: "home/informacoes-de-cuidado",
-        element: <InformacoesDeCuidado />,
-    },
-    {
-        path: "*",
-        element: <PaginaNaoEncontrada />,
-    },
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "home",
+    element: <Home />,
+  },
+  {
+    path: "home/profile",
+    element: <Profile />,
+  },
+  {
+    path: "home/informacoes-de-cuidado",
+    element: <InformacoesDeCuidado />,
+  },
+  {
+    path: "*",
+    element: <PaginaNaoEncontrada />,
+  },
 ]);
