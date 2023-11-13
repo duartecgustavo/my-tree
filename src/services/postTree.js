5e4773316b39161f0de02d39f92bd25360ee24b5
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2023-11-12 22:20:44 -0300

    adicionado parte de criar arvore

--- src/services/postTree.js
+++ src/services/postTree.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-three-api.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
